3b30eb77c2c5b3bf44da8806ed0366d87afc7544
Syncdelic <arrc.psy@gmail.com>
2024-11-30 15:33:45 -0600

    chore: 0.0.0.0

--- app.py
+++ app.py
@@ -31,5 +31,5 @@ def list_all_invoices_endpoint():
         return jsonify({"success": False, "error": str(e)}), 400
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
 
